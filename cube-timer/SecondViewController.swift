55ae43f1e7e12f1a1d74b794af8015c453224791
Anshul Ahuja <anshul.ahu@gmail.com>
2019-04-22 13:50:07 +0530

    Initial Commit

---
+++ cube-timer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  cube-timer
+//
+//  Created by Anshul Ahuja on 22/04/19.
+//  Copyright © 2019 Anshul Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cube-timerTests/cube_timerTests.swift
@@ -0,0 +1,34 @@
+//
+//  cube_timerTests.swift
+//  cube-timerTests
+//
+//  Created by Anshul Ahuja on 22/04/19.
+//  Copyright © 2019 Anshul Ahuja. All rights reserved.
+//
+
+import XCTest
+@testable import cube_timer
+
+class cube_timerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
