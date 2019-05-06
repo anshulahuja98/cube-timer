f450dee8dc3cb3eb7b09e95fb2287af71fa615a7
Anshul Ahuja <anshul.ahu@gmail.com>
2019-05-06 23:25:27 +0530

    Create logic for simultaneous button click

--- cube-timer/FirstViewController.swift
+++ cube-timer/FirstViewController.swift
@@ -10,6 +10,51 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var timer: UILabel!
+    @IBOutlet weak var trigger_checker: UILabel!
+    var left_button:Bool = false
+    var right_button:Bool = false
+    var timer_status = false
+    
+    func toggle_timer(){
+        if(timer_status){
+            stop_timer()
+        }
+        else{
+            start_timer()
+        }
+        
+    }
+    func stop_timer(){
+        timer_status = false
+    }
+    func start_timer(){
+        timer_status = true
+    }
+    
+    
+    @IBAction func right_button_down(_ sender: Any) {
+        right_button = true
+    }
+    
+    @IBAction func left_button_down(_ sender: Any) {
+        left_button = true
+    }
+    
+    @IBAction func right_button_up(_ sender: Any) {
+        if(right_button && left_button){
+            toggle_timer()
+        }
+        right_button = false
+    }
+    @IBAction func left_button_up(_ sender: Any) {
+        if(right_button && left_button){
+            toggle_timer()
+        }
+        left_button = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
